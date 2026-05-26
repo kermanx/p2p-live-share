96802397fef5262168dae474b4e3cf2d193a74bd
Marlon Bento Azevedo <marlon_lion@hotmail.com>
2026-05-26 16:37:59 -0300

    resquicios do terminal removidos

--- src/terminal/pty/shims/node-pty.ts
+++
@@ -1,4 +0,0 @@
-// import { resolveAsset } from './utils'
-
-// // eslint-disable-next-line ts/no-require-imports
-// module.exports = require(resolveAsset('node-pty/lib/index.js'))
--- src/terminal/pty/shims/windows-process-tree.ts
+++
@@ -1,4 +0,0 @@
-// import { resolveAsset } from './utils'
-
-// // eslint-disable-next-line ts/no-require-imports
-// module.exports = require(resolveAsset('@vscode/windows-process-tree/lib/index.js'))
